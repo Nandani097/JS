948fcdddefeacba319027bcaa484f0a03f9ef4fc
Nandani Gohel <nandanigohel09@gmail.com>
2025-07-15 04:55:43 +0000

    Variables &  Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/*alert(3 + 3) // we are using nodejs, not browser
+
+ console.log(3
+     + 
+    3) // code readability should be high
+*/
+ console.log("Nandani")
+
+ let name = "Nandani"
+ let age = 20
+ let isLoggedIn = false
+ let state = null
+
+ // number => 2 to power 53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined => 
+ // symbol => unique
+
+
+ // object
+
+ console.log(typeof "Nandani");
+ console.log(typeof null);
+ console.log(typeof undefined);
