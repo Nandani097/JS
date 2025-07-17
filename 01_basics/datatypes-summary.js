9ad454740b2c57df23d63ffd82c2f65009cda534
Nandani Gohel <nandanigohel09@gmail.com>
2025-07-17 12:38:57 +0000

    Update Datatype

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,7 +10,28 @@ const outsideTemp = null
 let userEmail;
 
 const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumer = 2153346557688888888867n
+
+
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Nandani",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof bigNumber);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
