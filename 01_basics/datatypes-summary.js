e1b08aa9a38935db46175eedd3929ab9e2be5f96
Nandani Gohel <nandanigohel09@gmail.com>
2025-07-17 13:06:24 +0000

    summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,29 @@ const myFunction = function(){
 console.log(typeof bigNumber);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// stack (Primitive), Heap (Non-Primitive) 
+
+let myYoutubename = "Nandanigoheldotcom"
+
+let anothername = myYoutubename
+anothername = "Nandu"
+
+console.log(anothername);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "nandani@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
