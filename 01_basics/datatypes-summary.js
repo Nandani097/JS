c789f123d728d120e6324b794fd06bb8ea1be1fb
Nandani Gohel <nandanigohel09@gmail.com>
2025-07-17 12:20:01 +0000

    Datatype

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt, 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
